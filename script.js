7b008d0ad5f307bc7d2c58f1494bc6a0350b7d42
kosbys <kosmicabysslord@protonmail.com>
2022-05-28 17:13:50 +0300

    Make slider responsive

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const container = document.querySelector(".grid");
+start();
+
+const cells = document.querySelectorAll(".cell");
+
+const reset = document.querySelector(`.reset`);
+
+const slider = document.querySelector(`.slider`);
 
 function start(size = 16) {
+  const container = document.querySelector(".grid");
   container.style.cssText = `display: grid;
     grid-template-columns: repeat(${size}, ${960 / size}px);
     grid-template-rows: repeat(${size}, ${960 / size}px);`;
@@ -14,12 +21,27 @@ function start(size = 16) {
   }
 }
 
-start();
+const resetGrid = function (cells) {
+  cells.forEach((cell) => {
+    cell.style.cssText = "background-color: white";
+  });
+};
 
-const cells = document.querySelectorAll(".cell");
+const updateSlider = function () {
+  const sliderText = document.querySelector(`.slider-text`);
+  sliderText.innerText = `Current size: ${slider.value} ✕ ${slider.value}`;
+};
 
 cells.forEach((cell) => {
   cell.addEventListener("mouseover", () => {
     cell.style.cssText = "background-color: black;";
   });
+});
+
+reset.addEventListener("click", () => {
+  resetGrid(cells);
+});
+
+slider.addEventListener("input", () => {
+  updateSlider();
 });
